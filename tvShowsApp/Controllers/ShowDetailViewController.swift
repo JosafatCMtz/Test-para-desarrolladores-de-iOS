1f7d139ac26fff47e8dd42069fdfea71df7bc971
Josafat Mtz <josamtz@outlook.com>
2021-01-11 16:39:56 -0600

    Adding the view controllers for each table 🛠

---
+++ tvShowsApp/Controllers/ShowDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ShowDetailViewController.swift
+//  tvShowsApp
+//
+//  Created by Josafat Martínez  on 11/01/21.
+//
+
+import UIKit
+
+class ShowDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
